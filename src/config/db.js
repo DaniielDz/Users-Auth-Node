11e87c9e10f730886ca226aadc6b8a90fd120cd1
DaniielDz <daniel13cba2002@gmail.com>
2025-03-14 15:05:48 -0300

    add database port

--- src/config/db.js
+++ src/config/db.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: process.env.DB_PORT,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
